d261155e39be53cd6c67bb043bedae37aaad8752
Jose Pina <jose.edpina@gmail.com>
2023-02-02 23:58:25 -0700

    Added enemy combatants to the game using object array and for loop to iterate each enemy in a round.

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,11 @@ var playerMoney = 10;
 // You can also log multiple values at once like this:
 console.log(playerName, playerHealth, playerAttack);
 
-var enemyName = 'Roborto';
+var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+var fight = function (enemyName) {
     // Alert the players that they are starting a round.
     window.alert('Welcome to Robot Gladiators!');
     // Ask the player if they would like to fight or skip the fight.
@@ -66,4 +66,6 @@ var fight = function () {
     }
 };
 
-fight();
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
