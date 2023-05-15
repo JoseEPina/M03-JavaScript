ee2fba04d4b587944181c4cf84ce8f99cff1dde1
Jose Pina <jose.edpina@gmail.com>
2023-05-15 12:12:17 +0200

    Added ability to randomize the fight order of the game using Math.random() method. Also added window alerts to better keep track of whose turn it is.

--- assets/js/game.js
+++ assets/js/game.js
@@ -92,67 +92,88 @@ var fightOrSkip = function () {
 };
 
 var fight = function (enemy) {
-    //console.log(enemy);
+    console.log(enemy);
+
+    // *Keep track of who will go first. (Variable is initialized with a value of true to start)
+    var isPlayerTurn = true;
+
+    // *Randomly change the turn order
+    if (Math.random() > 0.5) {
+        isPlayerTurn = false;
+    }
+
     // Repeat and execute as long as the enemy-robot is alive
     while (playerInfo.health > 0 && enemy.health > 0) {
-        // ask the player if they'd like to fight or skin using the fightOrSkip function
-        if (fightOrSkip()) {
-            // if True, leave fight by breaking loop
-            break;
-        }
-        // generate random damage value based on the player's attack power
-        var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-        // Remove enemy's health by subtracting the amount set in the damage variable.
-        enemy.health = Math.max(0, enemy.health - damage);
-        // Log a resulting message to the console so we know that it worked.
-        console.log(
-            playerInfo.name +
-                ' attacked ' +
-                enemy.name +
-                '. ' +
-                enemy.name +
-                ' now has ' +
-                enemy.health +
-                ' heath remaining.'
-        );
+        // ! Check whose turn it is. IF TRUE, then prompt the player to fight or skip.
+        if (isPlayerTurn) {
+            // ask the player if they'd like to fight or skin using the fightOrSkip function
+            if (fightOrSkip()) {
+                // if True, leave fight by breaking loop
+                break;
+            }
 
-        // check enemy's health
-        if (enemy.health <= 0) {
-            window.alert(enemy.name + ' has died!');
+            // generate random damage value based on the player's attack power
+            var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
-            // Award player money for winning!
-            playerInfo.money = playerInfo.money + 20;
-            console.log('playerInfo.money is now $' + playerInfo.money);
-
-            // Leave while() loop since enemy is dead
-            break;
-        } else {
-            window.alert(enemy.name + ' still has ' + enemy.health + ' health remaining.');
-        }
-
-        var damage = randomNumber(enemy.attack - 3, enemy.attack);
-
-        // Remove player's health by subtracting the amount set in the enemy.attack variable.
-        playerInfo.health = Math.max(0, playerInfo.health - damage);
-        // Log a resulting message to the console so we know that it worked.
-        console.log(
-            enemy.name +
-                ' attacked ' +
+            // Remove enemy's health by subtracting the amount set in the damage variable.
+            enemy.health = Math.max(0, enemy.health - damage);
+            // Log a resulting message to the console so we know that it worked.
+            window.alert(playerInfo.name + ' attacked ' + enemy.name + '.');
+            console.log(
                 playerInfo.name +
-                '. ' +
-                playerInfo.name +
-                ' now has ' +
-                playerInfo.health +
-                ' heath remaining.'
-        );
-        // check player's health
-        if (playerInfo.health <= 0) {
-            window.alert(playerInfo.name + ' has died!');
-            // Leave while() loop is player is dead
-            break;
+                    ' attacked ' +
+                    enemy.name +
+                    '. ' +
+                    enemy.name +
+                    ' now has ' +
+                    enemy.health +
+                    ' heath remaining.'
+            );
+
+            // check enemy's health
+            if (enemy.health <= 0) {
+                window.alert(enemy.name + ' has died!');
+
+                // Award player money for winning!
+                playerInfo.money = playerInfo.money + 20;
+                console.log('playerInfo.money is now $' + playerInfo.money);
+
+                // Leave while() loop since enemy is dead
+                break;
+            } else {
+                window.alert(enemy.name + ' still has ' + enemy.health + ' health remaining.');
+            }
+
+            // ! ELSE, player gets attacked first by robot enemy
         } else {
-            window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health remaining.');
+            var damage = randomNumber(enemy.attack - 3, enemy.attack);
+
+            // Remove player's health by subtracting the amount set in the enemy.attack variable.
+            playerInfo.health = Math.max(0, playerInfo.health - damage);
+            // Log a resulting message to the console so we know that it worked.
+            window.alert(enemy.name + ' attacked ' + playerInfo.name + '. ');
+            console.log(
+                enemy.name +
+                    ' attacked ' +
+                    playerInfo.name +
+                    '. ' +
+                    playerInfo.name +
+                    ' now has ' +
+                    playerInfo.health +
+                    ' heath remaining.'
+            );
+
+            // check player's health
+            if (playerInfo.health <= 0) {
+                window.alert(playerInfo.name + ' has died!');
+                // Leave while() loop is player is dead
+                break;
+            } else {
+                window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health remaining.');
+            }
         }
+        // *Switch turn order for next turn
+        isPlayerTurn = !isPlayerTurn;
     }
 };
 
@@ -226,7 +247,7 @@ var shop = function () {
         'Would you like to REFILL your health, UPGRADE your attack or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE or 3 to LEAVE.'
     );
 
-    // built-in function to convert string number into a integer number      
+    // built-in function to convert string number into a integer number
     shopOptionPrompt = parseInt(shopOptionPrompt);
 
     // use switch to carry out the action
